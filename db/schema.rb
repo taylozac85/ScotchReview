4f889baa6169c1b52e320c1287617a7f42fa11c5
Zach Taylor <taylozac85@gmail.com>
2013-02-05 11:26:22 -0800

    Refine reviews model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130205181707) do
+ActiveRecord::Schema.define(:version => 20130205184522) do
 
   create_table "drinks", :force => true do |t|
     t.string   "drink_name"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(:version => 20130205181707) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
+
+  add_index "reviews", ["user_id", "created_at"], :name => "index_reviews_on_user_id_and_created_at"
 
   create_table "users", :force => true do |t|
     t.string   "user_name"
--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -15,5 +15,10 @@ namespace :db do
                    password: password,
                    password_confirmation: password)
     end
+    users = User.all(limit: 6)
+    50.times do
+      review = Faker::Lorem.sentence(5)
+      users.each { |user| user.reviews.create!(:review => review) }
+    end
   end
 end
